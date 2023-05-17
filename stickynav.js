c111e47fa3bf63c9e8e471c5b68abec52284064c
Saimoan <iderrillix@gmail.com>
2023-05-17 23:39:25 +0800

    fixed navbar hover

--- stickynav.js
+++ stickynav.js
@@ -5,7 +5,7 @@ window.addEventListener("scroll", function() {
     console.log(hamburger.classList);
     var header = document.querySelector('header');
     header.classList.toggle("scrolled", window.scrollY > 0);
-    var navItemAnchor = document.getElementsByTagName('a');
+    var navItemAnchor = document.querySelectorAll('header .nav-links ul li a');
     for(let i = 0; i < navItemAnchor.length; i++){
         navItemAnchor[i].classList.toggle("nav-item-scrolled", window.scrollY > 0);
     }
